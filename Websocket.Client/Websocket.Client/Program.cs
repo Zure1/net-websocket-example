9cd54d03a91ecb4a925289b0e392f1442d0c004d
Zure <Luchsia@Freenet.de>
2023-08-21 18:11:02 +0200

    Add initial working example of Websocket.Server and Websocket.Client

---
+++ Websocket.Client/Websocket.Client/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Websocket.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string port = "5070";
+
+            Console.WriteLine("Press 'Enter' to continue...");
+            Console.ReadLine();
+
+            using (var websocketClient = new ClientWebSocket())
+            {
+                var serviceUri = new Uri($"ws://localhost:{port}/send");
+                var cancellationTokenSource = new CancellationTokenSource();
+                cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(120));
+
+                try
+                {
+                    await EstablishWebsocketConnectionAsync(websocketClient, serviceUri, cancellationTokenSource.Token);
+                    await HandleWebsocketCommunicationAsync(websocketClient, cancellationTokenSource.Token);
+                }
+                catch (WebSocketException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static async Task EstablishWebsocketConnectionAsync(ClientWebSocket websocketClient, Uri serviceUri, CancellationToken cancellationToken)
+        {
+            await websocketClient.ConnectAsync(serviceUri, cancellationToken);
+            Console.WriteLine("Websoket connection established!");
+        }
+
+        static async Task HandleWebsocketCommunicationAsync(ClientWebSocket clientWebSocket, CancellationToken cancellationToken)
+        {
+            while (clientWebSocket.State == WebSocketState.Open)
+            {
+                Console.WriteLine("Enter Message to send: ");
+                var message = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(message))
+                {
+                    await SendWebsocketMessage(clientWebSocket, message, cancellationToken);
+                    await ReceiveAndPrintWebsocketResponse(clientWebSocket, cancellationToken);
+                }
+            }
+        }
+
+        static async Task SendWebsocketMessage(ClientWebSocket clientWebSocket, string message, CancellationToken cancellationToken)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            ArraySegment<byte> bytesToSend = new ArraySegment<byte>(messageBytes);
+            await clientWebSocket.SendAsync(bytesToSend, WebSocketMessageType.Text, true, cancellationToken);
+        }
+
+        static async Task ReceiveAndPrintWebsocketResponse(ClientWebSocket clientWebSocket, CancellationToken cancellationToken)
+        {
+            byte[] responseBuffer = new byte[1024];
+            int offset = 0;
+            int packet = 1024;
+
+            while (true)
+            {
+                var bytesReceived = new ArraySegment<byte>(responseBuffer, offset, packet);
+                var response = await clientWebSocket.ReceiveAsync(bytesReceived, cancellationToken);
+                var responseMessage = Encoding.UTF8.GetString(responseBuffer, offset, response.Count);
+                Console.WriteLine(responseMessage);
+                if (response.EndOfMessage)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Websocket.Server/Websocket.Server/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Websocket.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            var wsOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120)
+            };
+
+            app.UseWebSockets(wsOptions);
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/send")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                        {
+                            await SendAsync(context, webSocket);
+                        }
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                }
+
+                // Call the next delegate to continue the pipeline (optional)
+                if (next != null)
+                {
+                    await next(context);
+                }
+            });
+        }
+
+        private async Task SendAsync(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            if (result != null)
+            {
+                while (!result.CloseStatus.HasValue)
+                {
+                    var message = Encoding.UTF8.GetString(new ArraySegment<byte>(buffer, 0, result.Count));
+                    Console.WriteLine($"Client says: {message}");
+                    await webSocket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes($"Server says: {DateTime.UtcNow:f} ")), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+            }
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
